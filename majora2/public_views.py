1008df7bd4105200643750780f8e7716de2e7dbd
Sam Nicholls <samstudio8@gmail.com>
2020-03-17 14:52:41 +0000

    COUNTERS

--- majora2/public_views.py
+++ majora2/public_views.py
@@ -5,7 +5,11 @@ from django.db.models import Count
 from . import models
 def sample_sequence_count_dashboard(request):
     collections = models.BiosourceSamplingProcess.objects.values("collection_by").annotate(Count("collection_by")).order_by("-collection_by__count")
+    total_collections = models.BiosourceSamplingProcess.objects.count()
 
     return render(request, 'public/special/dashboard.html', {
         "collections": collections,
+        "total_collections": total_collections,
+        "sequences": [],
+        "total_sequences": 0,
     })
