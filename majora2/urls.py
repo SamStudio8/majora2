3bd382e0a32a7bbe3ea7bc25e7a47124bff9770e
Sam Nicholls <samstudio8@gmail.com>
2020-03-22 12:38:31 +0000

    disable api v1 views

--- majora2/urls.py
+++ majora2/urls.py
@@ -26,18 +26,7 @@ urlpatterns = [
 
     path('accounts/profile/', views.profile, name='profile'),
 
-    # API
-    #path(r'api/command/update/$', csrf_exempt(views.update_command), name='update_command'),
-    #path(r'api/command/new/$', csrf_exempt(views.new_command), name='new_command'),
-    #path(r'api/resource/meta/$', csrf_exempt(views.tag_meta), name='tag_meta'),
-    #path(r'api/resource/group/$', csrf_exempt(views.group_resources), name='group_resources'),
-
-    path('api/checkin/', csrf_exempt(views.api_checkin_tube), name='api_checkin'),
-    path('api/extract/', csrf_exempt(views.api_extract), name='api_extract'),
-    #path('api/pipeline/', csrf_exempt(views.api_add_pipeline), name='api_pipeline'),
-
-    path('api/tubecontainer/add/', csrf_exempt(views.api_checkin_container), name="api_add_tubecontainer"),
-
+    # DATAMATRIX ###############################################################
     path('dm/<uuid:uuid>/', views.barcode, name="barcode"),
 
     # FORMS ####################################################################
@@ -45,9 +34,9 @@ urlpatterns = [
     path('forms/register/', account_views.form_register, name='form_register'),
     
     # OCARINA ##################################################################
-    path('ocarina/api/command/update/', csrf_exempt(views.ocarina_update_command), name='ocarina_update_command'),
-    path('ocarina/api/command/new/', csrf_exempt(views.ocarina_new_command), name='ocarina_new_command'),
-    path('ocarina/api/group/view/', csrf_exempt(views.ocarina_view_group), name='ocarina_view_group'),
+    #path('ocarina/api/command/update/', csrf_exempt(views.ocarina_update_command), name='ocarina_update_command'),
+    #path('ocarina/api/command/new/', csrf_exempt(views.ocarina_new_command), name='ocarina_new_command'),
+    #path('ocarina/api/group/view/', csrf_exempt(views.ocarina_view_group), name='ocarina_view_group'),
 
     # BOT ######################################################################
     path('bot/accounts/approve', csrf_exempt(bot_views.bot_approve_registration)),
@@ -64,6 +53,10 @@ urlpatterns = [
     # NEW API
     path('api/v2/artifact/biosample/add/', api_views.add_biosample, name="api.artifact.biosample.add"),
     path('api/v2/artifact/digitalresource/add/', api_views.add_digitalresource, name="api.artifact.digitalresource.add"),
+    path('api/checkin/', csrf_exempt(views.api_checkin_tube), name='api_checkin'),
+    path('api/extract/', csrf_exempt(views.api_extract), name='api_extract'),
+
+    path('api/tubecontainer/add/', csrf_exempt(views.api_checkin_container), name="api_add_tubecontainer"),
 
 
     # Home
