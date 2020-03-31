669d20b18e15d2456ea768f7111694d5e391cce6
Sam Nicholls <samstudio8@gmail.com>
2020-03-31 18:25:36 +0000

    public dashboard

--- majora2/public_views.py
+++ majora2/public_views.py
@@ -11,9 +11,13 @@ def sample_sequence_count_dashboard(request):
     consensii = models.DigitalResourceArtifact.objects.filter(current_kind="consensus").values(org=F("created__who__profile__institute__name")).annotate(count=Count("org"))
     total_consensii = models.DigitalResourceArtifact.objects.filter(current_kind="consensus").count()
 
+    adm2 = models.BiosourceSamplingProcess.objects.all().values(adm2=F("collection_location_adm2")).annotate(count=Count("adm2")).order_by("adm2")
+
     return render(request, 'public/special/dashboard.html', {
         "collections": collections,
         "total_collections": total_collections,
         "sequences": consensii,
         "total_sequences": total_consensii,
+        "adm2": adm2,
+        "n_regions": len(adm2),
     })
