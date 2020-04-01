b5e7a4a63897cadf9a4d939cbcec842de64c3a8b
Sam Nicholls <samstudio8@gmail.com>
2020-04-01 15:24:30 +0000

    tidy sparks

--- majora2/public_views.py
+++ majora2/public_views.py
@@ -5,36 +5,11 @@ from django.db.models import F
 
 from django.utils import timezone
 from django.db.models.functions import TruncDay
-from dateutil.rrule import rrule, DAILY
 import datetime
-import json
-
-def test():
-    a_count = models.DigitalResourceArtifact.objects.filter(created__when__isnull=False, created__when__gte=timezone.now().date()-datetime.timedelta(days=30)).annotate(date=TruncDay('created__when')).values("date").annotate(created_count=Count('id')).order_by("date")
-    counts = []
-    i = 0
-    n = 0
-    cursor = a_count[i]
-    for dt in rrule(DAILY, dtstart=timezone.now().date()-datetime.timedelta(days=30), until=timezone.now().date()):
-        dt = dt.date()
-        cdt = cursor["date"].date()
-        if dt < cdt:
-            counts.append({"date": dt.strftime("%Y-%m-%d"), "count": n})
-            continue
-        elif dt == cdt:
-            n += cursor["created_count"]
-            i += 1
-            counts.append({"date": dt.strftime("%Y-%m-%d"), "count": n})
-            try:
-                cursor = a_count[i]
-            except IndexError:
-                pass
-        elif dt > cdt:
-            counts.append({"date": dt.strftime("%Y-%m-%d"), "count": n})
-
-    return counts
 
 from . import models
+from . import util
+
 def sample_sequence_count_dashboard(request):
     collections = models.BiosourceSamplingProcess.objects.values("collected_by").annotate(Count("collected_by")).order_by("-collected_by__count")
     total_collections = models.BiosourceSamplingProcess.objects.count()
@@ -44,6 +19,7 @@ def sample_sequence_count_dashboard(request):
 
     adm2 = models.BiosourceSamplingProcess.objects.all().values(adm2=F("collection_location_adm2")).annotate(count=Count("adm2")).order_by("adm2")
 
+    consensus_spark = util.make_spark(models.DigitalResourceArtifact.objects.filter(created__when__isnull=False, created__when__gte=timezone.now().date()-datetime.timedelta(days=30)).annotate(date=TruncDay('created__when')).values("date").annotate(count=Count('id')).order_by("date"), days=30)
 
 
     return render(request, 'public/special/dashboard.html', {
@@ -53,5 +29,6 @@ def sample_sequence_count_dashboard(request):
         "total_sequences": total_consensii,
         "adm2": adm2,
         "n_regions": len(adm2),
-        "counts": test(),
+
+        "consensus_spark": consensus_spark,
     })
--- majora2/util.py
+++ majora2/util.py
@@ -1,4 +1,7 @@
 from . import models
+from dateutil.rrule import rrule, DAILY
+import datetime
+from django.utils import timezone
 
 def quarantine_artifact(process, artifact):
     artifact.quarantined = True
@@ -13,3 +16,27 @@ def quarantine_artifact(process, artifact):
 
 def create_biosample(self):
     pass
+
+
+def make_spark(queryset, days=30):
+    counts = []
+    i = 0
+    n = 0
+    cursor = queryset[i]
+    for dt in rrule(DAILY, dtstart=timezone.now().date()-datetime.timedelta(days=days), until=timezone.now().date()):
+        dt = dt.date()
+        cdt = cursor["date"].date()
+        if dt < cdt:
+            counts.append({"date": dt.strftime("%Y-%m-%d"), "count": n})
+            continue
+        elif dt == cdt:
+            n += cursor["count"]
+            i += 1
+            counts.append({"date": dt.strftime("%Y-%m-%d"), "count": n})
+            try:
+                cursor = queryset[i]
+            except IndexError:
+                pass
+        elif dt > cdt:
+            counts.append({"date": dt.strftime("%Y-%m-%d"), "count": n})
+    return counts
