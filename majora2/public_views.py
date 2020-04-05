33d208055fc9365ec63f65d0386fbf38b795ca82
Sam Nicholls <samstudio8@gmail.com>
2020-04-05 13:23:27 +0000

    show public count on dashboard too

--- majora2/public_views.py
+++ majora2/public_views.py
@@ -14,19 +14,31 @@ def sample_sequence_count_dashboard(request):
     collections = models.BiosourceSamplingProcess.objects.values("collected_by").annotate(Count("collected_by")).order_by("-collected_by__count")
     total_collections = models.BiosourceSamplingProcess.objects.count()
 
-    consensii = models.DigitalResourceArtifact.objects.filter(current_kind="consensus").values(org=F("created__who__profile__institute__name")).annotate(count=Count("org"))
-    total_consensii = models.DigitalResourceArtifact.objects.filter(current_kind="consensus").count()
-
     adm2 = models.BiosourceSamplingProcess.objects.all().values(adm2=F("collection_location_adm2")).annotate(count=Count("adm2")).order_by("adm2")
 
     consensus_spark = util.make_spark(models.DigitalResourceArtifact.objects.filter(created__when__isnull=False, created__when__gte=timezone.now().date()-datetime.timedelta(days=30)).annotate(date=TruncDay('created__when')).values("date").annotate(count=Count('id')).order_by("date"), days=30)
 
+    #consensus_spark = util.make_spark(models.DNASequencingProcess.objects.filter(when__gte=timezone.now().date()-datetime.timedelta(days=30)).annotate(date=TruncDay('when')).values("date").annotate(count=Count('id')).order_by("date"), days=30)
+    #total_consensii = models.DNASequencingProcess.objects.count()
+
+    total_c = 0
+    sites = {}
+    for c in models.DigitalResourceArtifact.objects.filter(current_kind="consensus"):
+        site_name = c.created.who.profile.institute.name
+        if site_name not in sites:
+            sites[site_name] = {"count": 0, "public": 0, "site": site_name}
+        sites[site_name]["count"] += 1
+        total_c += 1
+
+        is_public = c.cogtemp_get_public
+        if is_public == '1': # its a string lolololol ffs
+            sites[site_name]["public"] += 1
 
     return render(request, 'public/special/dashboard.html', {
         "collections": collections,
         "total_collections": total_collections,
-        "sequences": consensii,
-        "total_sequences": total_consensii,
+        "new_sequences": sorted(sites.items(), key=lambda x: x[1]["count"], reverse=True),
+        "total_sequences": total_c,
         "adm2": adm2,
         "n_regions": len(adm2),
 
