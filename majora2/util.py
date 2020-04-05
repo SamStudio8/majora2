b990ce6791cbafbb503613a762ec373ddda43133
Sam Nicholls <samstudio8@gmail.com>
2020-04-05 10:32:56 +0000

    safeguard spark

--- majora2/util.py
+++ majora2/util.py
@@ -34,21 +34,24 @@ def make_spark(queryset, days=30):
     counts = []
     i = 0
     n = 0
-    cursor = queryset[i]
-    for dt in rrule(DAILY, dtstart=timezone.now().date()-datetime.timedelta(days=days), until=timezone.now().date()):
-        dt = dt.date()
-        cdt = cursor["date"].date()
-        if dt < cdt:
-            counts.append({"date": dt.strftime("%Y-%m-%d"), "count": n})
-            continue
-        elif dt == cdt:
-            n += cursor["count"]
-            i += 1
-            counts.append({"date": dt.strftime("%Y-%m-%d"), "count": n})
-            try:
-                cursor = queryset[i]
-            except IndexError:
-                pass
-        elif dt > cdt:
-            counts.append({"date": dt.strftime("%Y-%m-%d"), "count": n})
+    try:
+        cursor = queryset[i]
+        for dt in rrule(DAILY, dtstart=timezone.now().date()-datetime.timedelta(days=days), until=timezone.now().date()):
+            dt = dt.date()
+            cdt = cursor["date"].date()
+            if dt < cdt:
+                counts.append({"date": dt.strftime("%Y-%m-%d"), "count": n})
+                continue
+            elif dt == cdt:
+                n += cursor["count"]
+                i += 1
+                counts.append({"date": dt.strftime("%Y-%m-%d"), "count": n})
+                try:
+                    cursor = queryset[i]
+                except IndexError:
+                    pass
+            elif dt > cdt:
+                counts.append({"date": dt.strftime("%Y-%m-%d"), "count": n})
+    except:
+        pass
     return counts
