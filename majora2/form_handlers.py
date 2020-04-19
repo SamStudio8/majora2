1e369913cc880d6f6cabd0fbfe57e26ae2ce7dd1
Sam Nicholls <samstudio8@gmail.com>
2020-04-19 13:32:43 +0000

    api show new pag

--- majora2/form_handlers.py
+++ majora2/form_handlers.py
@@ -395,6 +395,9 @@ def handle_testdigitalresource(form, user=None, api_o=None):
         )
         res.groups.add(pag)
         pag.save()
+        if pag_created and api_o:
+            api_o["new"].append(_format_tuple(pag))
+            api_o["updated"].append(_format_tuple(res))
 
     if created and api_o:
         api_o["new"].append(_format_tuple(res))
