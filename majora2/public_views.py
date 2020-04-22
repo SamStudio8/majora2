5b8a8dbe6234677153c5b83e7a94aa08fe826acc
Sam Nicholls <samstudio8@gmail.com>
2020-04-22 17:28:56 +0000

    count pags not sequences on dashboard

--- majora2/public_views.py
+++ majora2/public_views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.http import HttpResponse, HttpResponseBadRequest
-from django.db.models import Count
-from django.db.models import F
+from django.db.models import Count, F, Q
 from django.views.decorators.cache import cache_page
 
 from django.utils import timezone
@@ -20,27 +19,13 @@ def sample_sequence_count_dashboard(request):
 
     consensus_spark = util.make_spark(models.DigitalResourceArtifact.objects.filter(current_kind="consensus", created__when__isnull=False, created__when__gte=timezone.now().date()-datetime.timedelta(days=30)).annotate(date=TruncDay('created__when')).values("date").annotate(count=Count('id')).order_by("date"), days=30)
 
-    #consensus_spark = util.make_spark(models.DNASequencingProcess.objects.filter(when__gte=timezone.now().date()-datetime.timedelta(days=30)).annotate(date=TruncDay('when')).values("date").annotate(count=Count('id')).order_by("date"), days=30)
-    #total_consensii = models.DNASequencingProcess.objects.count()
-
-    total_c = 0
-    sites = {}
-    for c in models.DigitalResourceArtifact.objects.filter(current_kind="consensus"):
-        site_name = c.created.who.profile.institute.name
-        if site_name not in sites:
-            sites[site_name] = {"count": 0, "public": 0, "site": site_name}
-        sites[site_name]["count"] += 1
-        total_c += 1
-
-        is_public = c.cogtemp_get_public
-        if is_public == '1': # its a string lolololol ffs
-            sites[site_name]["public"] += 1
+    pags_by_site = models.PublishedArtifactGroup.objects.values(site=F('owner__profile__institute__name')).annotate(count=Count('pk'), public=Count('pk', filter=Q(is_public=True)), private=Count('pk', filter=Q(is_public=False))).order_by('-count')
+    total_pags = models.PublishedArtifactGroup.objects.count()
 
     return render(request, 'public/special/dashboard.html', {
-        #"collections": collections,
         "total_collections": total_collections,
-        "new_sequences": sorted(sites.items(), key=lambda x: x[1]["count"], reverse=True),
-        "total_sequences": total_c,
+        "total_sequences": total_pags,
+        "site_pags": pags_by_site,
         "adm2": adm2,
         "n_regions": len(adm2),
 
