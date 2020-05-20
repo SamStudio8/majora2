b3fb0aaefa6aea932bd757aace774be4d4d97096
Sam Nicholls <samstudio8@gmail.com>
2020-05-20 10:09:41 +0000

    supplemental model for cog collections

--- majora2/submodels/supplemental.py
+++ majora2/submodels/supplemental.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class COGUK_BiosourceSamplingProcessSupplement(models.Model):
-    sampling = models.ForeignKey('BiosourceSamplingProcess', on_delete=models.CASCADE, related_name="coguk_supp")
-    is_surveillance = models.BooleanField(null=False)
+    sampling = models.OneToOneField('BiosourceSamplingProcess', on_delete=models.CASCADE, related_name="coguk_supp")
+    is_surveillance = models.BooleanField(null=True)
     is_hcw = models.BooleanField(null=True)
     employing_hospital_name = models.CharField(max_length=100, blank=True, null=True)
     employing_hospital_trust_or_board = models.CharField(max_length=100, blank=True, null=True)
