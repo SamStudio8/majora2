3e3477674ead38f61d99789546434f6e36832191
Sam Nicholls <samstudio8@gmail.com>
2020-05-27 14:57:59 +0000

    fire a slack msg for new users still

--- majora2/receivers.py
+++ majora2/receivers.py
@@ -25,6 +25,40 @@ def recv_new_registration(sender, username, first_name, last_name, organisation,
         [p.user.email for p in site_admins],
         fail_silently=True,
     )
+    if settings.SLACK_CHANNEL:
+        slack_message('slack/blank', {
+        }, [{
+            "mrkdwn_in": ["text", "pretext", "fields"],
+            "title": "New user registration WAITING for approval by site",
+            "title_link": "",
+            "text": "%s %s (%s) requested account %s to be added" % (first_name, last_name, organisation, username),
+            "footer": "New user spotted by Majora",
+            "footer_icon": "https://avatars.slack-edge.com/2019-05-03/627972616934_a621b7d3a28c2b6a7bd1_512.jpg",
+
+            "fields": [
+                {
+                    "title": "Metadata",
+                    "short": False
+                },
+                {
+                    "title": "Organisation",
+                    "short": True
+                },
+                {
+                    "value": organisation,
+                    "short": True
+                },
+                {
+                    "title": "Approvers",
+                    "short": True
+                },
+                {
+                    "value": str(["%s %s" % (x.user.first_name, x.user.last_name) for x in site_admins]),
+                    "short": True
+                },
+            ],
+            "ts": int(time.time()),
+        }])
 
 @receiver(signals.new_sample)
 def recv_new_sample(sender, sample_id, submitter, **kwargs):
