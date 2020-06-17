a9be6b93005d8605b0259066b89a95956f2c7778
Sam Nicholls <samstudio8@gmail.com>
2020-06-17 10:20:23 +0000

    minimal quote for tsv

--- majora2/renderers.py
+++ majora2/renderers.py
@@ -1,8 +1,10 @@
 from rest_framework_csv import renderers as r
+import csv
 
 class TSVRenderer (r.CSVRenderer):
     media_type = "text/tsv"
     format = "tsv"
     writer_opts = dict(
+        quoting=csv.QUOTE_MINIMAL,
         delimiter='\t',
     )
