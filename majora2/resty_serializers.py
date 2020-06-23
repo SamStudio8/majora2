48a8c28734d6f3660ff207e47cbba57b599abec9
Sam Nicholls <samstudio8@gmail.com>
2020-06-23 16:14:45 +0000

    whatever

--- majora2/resty_serializers.py
+++ majora2/resty_serializers.py
@@ -8,10 +8,41 @@ class RestyArtifactSerializer(serializers.ModelSerializer):
         model = models.MajoraArtifact
         fields = ('id', 'dice_name', 'artifact_kind',)
 
+class RestyBiosourceSamplingProcessSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.BiosourceSamplingProcess
+        fields = (
+                'collection_date',
+                'received_date',
+                'source_age',
+                'source_sex',
+                'collection_location_country',
+                'collection_location_adm1',
+                'collection_location_adm2',
+                'private_collection_location_adm2',
+        )
+        extra_kwargs = {
+                'private_collection_location_adm2': {'write_only': True},
+        }
 class RestyBiosampleArtifactSerializer(RestyArtifactSerializer):
+    collection = RestyBiosourceSamplingProcessSerializer(source="created")
     class Meta:
         model = models.BiosampleArtifact
-        fields = RestyArtifactSerializer.Meta.fields + ('central_sample_id',)
+        fields = RestyArtifactSerializer.Meta.fields + (
+                'central_sample_id',
+                'sender_sample_id',
+                'root_sample_id',
+                'collection')
+        extra_kwargs = {
+                'root_sample_id': {'write_only': True},
+                'sender_sample_id': {'write_only': True}
+        }
+
+   #def create(self, validated_data):
+   #    modela_data = validated_data.pop('model_a')
+   #    model_b = ModelB.objects.create(**validated_data)
+   #    ModelA.objects.create(model_b=model_b, **modela_data)
+   #    return model_b
 
 class RestyArtifactSerializer(PolymorphicSerializer):
     resource_type_field_name = 'artifact_model'
--- majora2/resty_views.py
+++ majora2/resty_views.py
@@ -1,11 +1,13 @@
 from django.contrib.auth.decorators import login_required
 
+from rest_framework.settings import api_settings
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import generics
 from rest_framework import permissions
 from rest_framework import viewsets
+from rest_framework import mixins
 
 from two_factor.views.mixins import OTPRequiredMixin
 
@@ -17,7 +19,12 @@ class ArtifactDetail(generics.RetrieveAPIView):
     queryset = models.MajoraArtifact.objects.all()
     serializer_class = serializers.RestyArtifactSerializer
 
-class BiosampleView(viewsets.ModelViewSet):
+class BiosampleView(
+                    mixins.CreateModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.ListModelMixin,
+                    mixins.RetrieveModelMixin,
+                    viewsets.GenericViewSet):
     queryset = models.BiosampleArtifact.objects.all()
     serializer_class = serializers.RestyBiosampleArtifactSerializer
     #TODO permissions class
