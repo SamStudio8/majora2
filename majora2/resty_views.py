f01d0cc4ae81f8d1c9862bea1f5fb28d0ae3381d
Sam Nicholls <samstudio8@gmail.com>
2020-07-05 15:04:46 +0000

    celery task function as mixin

--- majora2/resty_views.py
+++ majora2/resty_views.py
@@ -13,14 +13,33 @@ from rest_framework import mixins
 
 from two_factor.views.mixins import OTPRequiredMixin
 
+from majora2 import tasks
 from majora2 import models
 from majora2 import resty_serializers as serializers
 from majora2.authentication import TatlTokenAuthentication
 
 import uuid
 
-class MajoraCeleryMixin(object):
-    pass
+class MajoraCeleryListingMixin(object):
+    def list(self, request, *args, **kwargs):
+        queryset = list(self.filter_queryset(self.get_queryset()).values_list('id', flat=True))
+
+        api_o = {}
+
+        if self.celery_task:
+            celery_task = self.celery_task.delay(queryset)
+            if celery_task:
+                api_o["errors"] = 0
+                api_o["test"] = request.query_params
+                api_o["expected_n"] = len(queryset)
+                api_o["tasks"] = celery_task.id
+                api_o["messages"] = "Call api.majora.task.get with the appropriate task ID later..."
+            else:
+                api_o["errors"] = 1
+                api_o["messages"] = "Could not add requested task to Celery..."
+            return Response(
+                api_o
+            )
 
 class MajoraUUID4orDiceNameLookupMixin(object):
     def get_object(self):
@@ -88,12 +107,15 @@ class TaskView(APIView):
 
 class PublishedArtifactGroupView(
                     MajoraUUID4orDiceNameLookupMixin,
-                    mixins.ListModelMixin,
+                    #mixins.ListModelMixin,
+                    MajoraCeleryListingMixin,
                     mixins.RetrieveModelMixin,
                     viewsets.GenericViewSet):
     serializer_class = serializers.RestyPublishedArtifactGroupSerializer
     majora_alternative_field = "published_name"
     queryset = models.PublishedArtifactGroup.objects.all()
+
+    celery_task = tasks.task_get_pag_by_qc_v3
 
     def get_queryset(self):
         queryset = self.queryset
@@ -117,23 +139,3 @@ class PublishedArtifactGroupView(
                 queryset = queryset.filter(is_public=False)
         return queryset
 
-    def xlist(self, request, *args, **kwargs):
-        #TODO This overrides the list function entirely which loses pagination
-        # there is probably a "correct" way to do this re:mixins
-        queryset = list(self.filter_queryset(self.get_queryset()).values_list('id', flat=True))
-
-        api_o = {}
-        from . import tasks
-        celery_task = tasks.task_get_pag_by_qc_v3.delay(queryset)
-        if celery_task:
-            api_o["errors"] = 0
-            api_o["test"] = request.query_params
-            api_o["expected_n"] = len(queryset)
-            api_o["tasks"] = celery_task.id
-            api_o["messages"] = "Call api.majora.task.get with the appropriate task ID later..."
-        else:
-            api_o["errors"] = 1
-            api_o["messages"] = "Could not add requested task to Celery..."
-        return Response(
-            api_o
-        )
