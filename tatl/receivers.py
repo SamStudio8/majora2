65b4a3693d5542142f2d9cff15b41b844d0b27a2
Sam Nicholls <samstudio8@gmail.com>
2020-08-07 14:02:04 +0000

    recv tatltask

--- tatl/receivers.py
+++ tatl/receivers.py
@@ -1,12 +1,16 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.conf import settings
+from django.core.mail import send_mail
+from django.utils import timezone
+
+from celery.signals import after_task_publish,task_success,task_prerun,task_postrun
+from django_slack import slack_message
+
+import json
 
 from . import models
-
-from django_slack import slack_message
-from django.core.mail import send_mail
-import json
+from majora2.models import Profile
 
 @receiver(post_save, sender=models.TatlPermFlex)
 def announce_perm_flex(sender, instance, **kwargs):
@@ -29,3 +33,34 @@ def announce_perm_flex(sender, instance, **kwargs):
                 ext,
             ),
         }])
+
+@task_prerun.connect()
+def task_prerun(signal=None, sender=None, task_id=None, task=None, args=None, **kwargs):
+    kwargs = kwargs.get("kwargs") # Don't ask
+
+    treq = None
+    if kwargs.get("response_uuid"):
+        treq = models.TatlRequest.objects.get(response_uuid=kwargs.get("response_uuid"))
+
+    tuser = None
+    if kwargs.get("user"):
+        tuser = Profile.objects.get(user__pk=kwargs.get("user"))
+        tuser = tuser.user
+
+    ttask = models.TatlTask(
+        celery_uuid = task_id,
+        task = task.name,
+        payload = json.dumps(kwargs),
+        timestamp = timezone.now(),
+        request = treq,
+        user = tuser,
+    )
+    ttask.save()
+
+@task_postrun.connect()
+def task_postrun(signal=None, sender=None, task_id=None, task=None, args=None, retval=None, state=None, **kwargs):
+    ttask = models.TatlTask.objects.get(celery_uuid=task_id)
+    now = timezone.now()
+    ttask.response_time = now - ttask.timestamp
+    ttask.state = state
+    ttask.save()
