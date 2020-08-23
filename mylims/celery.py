44be27261c115acf9e084cd631d21eb72935e6a3
Sam Nicholls <samstudio8@gmail.com>
2020-08-23 12:53:21 +0000

    override celery trace to suppress potential for sensitive stuff to go to celery logger

--- mylims/celery.py
+++ mylims/celery.py
@@ -3,11 +3,16 @@ from __future__ import absolute_import, unicode_literals
 import os
 
 from celery import Celery
+from celery.app import trace
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mylims.settings')
 
 app = Celery('mylims')
+
+trace.LOG_SUCCESS = """\
+Task %(name)s[%(id)s] succeeded in %(runtime)ss\
+"""
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
@@ -17,7 +22,6 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
-
 
 @app.task(bind=True)
 def debug_task(self):
