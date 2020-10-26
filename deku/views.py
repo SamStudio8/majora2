7bec8a9d704de604c538e0c79f46d57390424c68
Sam Nicholls <samstudio8@gmail.com>
2020-10-26 18:10:33 +0000

    sprinkle datatable magic over deku table

--- deku/views.py
+++ deku/views.py
@@ -23,6 +23,7 @@ def list_all_profiles(request):
     inactive_site_profiles = models.Profile.objects.filter(is_site_approved=False)
     return render(request, 'list_all_profiles.html', {
         'user': request.user,
+        "site_codes": sorted(models.Institute.objects.all().values_list('code', flat=True)),
         'active_profiles': active_site_profiles,
         'inactive_profiles': inactive_site_profiles,
     })
