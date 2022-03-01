abab9c7a18a335bc999879cfc4417ebff831b089
Sam Nicholls <sam@samnicholls.net>
2022-03-01 17:56:18 +0000

    Majora Version Footer

---
+++ majora2/context_processors.py
@@ -0,0 +1,9 @@
+import os
+
+# Some people access the file system or us processes to read git
+# I think that is a bit creepy so lets just read the ENV
+def majora_version(request):
+    return {
+        "majora_version": os.getenv("CURRENT_MAJORA_VERSION"),
+        "majora_commit": os.getenv("CURRENT_MAJORA_HASH")
+    }
--- majora2/templatetags/instance.py
+++ majora2/templatetags/instance.py
@@ -12,6 +12,10 @@ def footer():
     ))
 
 @register.simple_tag
+def mask():
+    return mark_safe('<img src="/static/mask_50.png" height="15px" alt="Mask of Majora" title="You\'ve met with a terrible fate, haven\'t you?" />')
+
+@register.simple_tag
 def instance_name():
     return mark_safe(getattr(settings, "INSTANCE_NAME", ""))
 
