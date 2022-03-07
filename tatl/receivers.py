7a7eabf2b2bf7cd4965b71cdf5d90b706213fb4a
Sam Nicholls <sam@samnicholls.net>
2022-03-07 01:26:57 +0000

    Remove dead files

--- tatl/receivers.py
+++ tatl/receivers.py
@@ -1,7 +1,6 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.conf import settings
-from django.core.mail import send_mail
 from django.utils import timezone
 
 
