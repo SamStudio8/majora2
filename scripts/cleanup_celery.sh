4932bbedb39748c2ae05462d116b703a12e56c47
Sam Nicholls <samstudio8@gmail.com>
2022-03-07 01:49:56 +0000

    take out the trash more often

--- scripts/cleanup_celery.sh
+++ scripts/cleanup_celery.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-find /var/www/majora/private/celery_results -type f -daystart -mtime +7 -exec rm -rf {} \;
+find /var/www/majora/private/celery_results -type f -daystart -mtime +2 -exec rm -rf {} \;
